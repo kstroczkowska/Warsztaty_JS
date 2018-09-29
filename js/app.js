c60075211f4e8e8192e9a3fdfe98b9aeed5c8e70
kstroczkowska <kchmielewska90@gmail.com>
2018-09-29 14:48:52 +0200

    commit numer 4

--- js/app.js
+++ js/app.js
@@ -10,6 +10,29 @@ var subMenu = document.querySelector ('.dropdown');
         subMenu.style.display = 'none';
         
        }); 
+    var buttons = document.querySelectorAll('.read-more');
+    //console.log/buttons[0].
+    
+    function showHide() { var textArea = this.previousElementSibling;
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else
+      {textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+        
+    }
+    
+    for (var i = 0; i <= buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
+    
+    
+      
+
+  
+     
 });
              
 
